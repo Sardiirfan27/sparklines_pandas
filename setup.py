492b7bb353e2093368a2f0dcdd9625869062db3d
Sardi Irfansyah <64241627+Sardiirfan27@users.noreply.github.com>
2023-04-30 20:34:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
-
+from sparklines import __version__
 setup(
-    name='sparklines_pandas',
-    version='0.1.0',
+    name='sparklines',
+    version=__version__,
     description='creating sparklines on a pandas dataframe',
     url='https://github.com/sardiirfan27/sparklines_pandas',
     author='Sardi Irfansyah',
     author_email='sardiirfan27@gmail.com',
-    packages=['sparklines_pandas'],
+    packages=['sparklines'],
     install_requires=[
         'numpy',
         'pandas',
